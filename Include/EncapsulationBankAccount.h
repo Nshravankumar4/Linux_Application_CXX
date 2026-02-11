faf891819b6b7c5761630bb91c8968a68f525db6
NShravankumar4 <shravannunsavath@DESKTOP-9ASPK4E.localdomain>
2026-02-11 05:22:44 +0000

    updated

--- Include/EncapsulationBankAccount.h
+++ Include/EncapsulationBankAccount.h
@@ -33,7 +33,7 @@ public:
     // Getter function
     // -----------------------------
     // Returns the current balance of the account
-    double getBalance();
+    double getBalance() const;
 
     // -----------------------------
     // Safe withdrawal function
